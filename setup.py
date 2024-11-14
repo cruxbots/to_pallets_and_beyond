1cd35275adeb4aa1451c83c48541c10fa36f2a23
cruxbots <mishrarahul1997@yahoo.com>
2024-11-14 16:52:52 +0530

    creating ros package and writing dummy node

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'to_pallete_and_beyond'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rahul',
+    maintainer_email='mishrarahul1997@yahoo.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pallete_detector = to_pallete_and_beyond.pallete_detector:main'
+        ],
+    },
+)
